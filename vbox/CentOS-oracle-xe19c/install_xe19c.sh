3e431a0a930dfcd2998e5bd8c5dc1b1489b16ce9
ksa <ksa@orsk.ru>
2019-04-27 12:35:07 +0300

    !FIX SID name in Oracle 19c XE (vbox provider)

--- vbox/CentOS-oracle-xe19c/install_xe19c.sh
+++ vbox/CentOS-oracle-xe19c/install_xe19c.sh
@@ -17,34 +17,34 @@ echo 'INSTALLER: Oracle XE 19c install'
 yum -y localinstall /vagrant/oracle-database-ee-19c-1.0-1.x86_64.rpm
 
 echo 'INSTALLER: Oracle software installed'
-echo 'INSTALLER: Create database XE'
+echo 'INSTALLER: Create database ORCLCDB'
 
-(echo "GetStarted19c"; echo "GetStarted19c";) | /etc/init.d/oracle-xe-19c configure 
+(echo "GetStarted19c"; echo "GetStarted19c";) | /etc/init.d/oracledb_ORCLCDB-19c configure 
 
-echo 'INSTALLER: set oracle XE autostart'
+echo 'INSTALLER: set oracle ORCLCDB autostart'
 
 systemctl daemon-reload
-systemctl enable oracle-xe-19c
+systemctl enable oracledb_ORCLCDB-19c
 
 # Check installed RPM's
 rpm -qa | grep oracle
 
 cat > /home/oracle/setEnv.sh <<EOF 
 export ORACLE_HOME=/opt/oracle/product/19c/dbhomeXE
-export ORACLE_SID=XE
+export ORACLE_SID=ORCLCDB
 export PATH=\$ORACLE_HOME/bin:\$PATH
 EOF
 
 cat >> /home/oracle/.bash_profile <<EOF
-. /home/oracle/scripts/setEnv.sh
+. /home/oracle/setEnv.sh
 EOF
 
 # check DB connect
-export ORACLE_HOME=/opt/oracle/product/19c/dbhomeXE
-export ORACLE_SID=XE
-export PATH=$PATH:/opt/oracle/product/19c/dbhomeXE/bin
+export ORACLE_HOME=/opt/oracle/product/19c/dbhome_1
+export ORACLE_SID=ORCLCDB
+export PATH=$PATH:/opt/oracle/product/19c/dbhome_1/bin
 
-sqlplus sys/GetStarted19c@//localhost:1521/XE as sysdba <<EOF
+sqlplus sys/GetStarted19c@//localhost:1521/ORCLCDB as sysdba <<EOF
 select * from dual;
 quit
 EOF
