d98b4ceda6cea05d6e2bd7da2c3ef5e049ac2ee0
Serg K <ksa@orsk.ru>
2019-12-01 13:14:18 +0300

    +ADD vbox CentOS7 install PgSQL master-slave 4in1 ansible

--- vbox/CentOS7_PgSQL_ansible_3in1/scripts/slave.sh
+++
@@ -1,4 +0,0 @@
-#enable password auth
-sed -i -e "s|#ChallengeResponseAuthentication yes|ChallengeResponseAuthentication yes|g" /etc/ssh/sshd_config
-sed -i -e "s|ChallengeResponseAuthentication no|#ChallengeResponseAuthentication no|g" /etc/ssh/sshd_config
-service sshd restart
--- vbox/CentOS7_PgSQL_ansible_4in1/scripts/master.sh
+++ vbox/CentOS7_PgSQL_ansible_4in1/scripts/master.sh
@@ -26,11 +26,10 @@ chown -R vagrant:vagrant /home/vagrant/
 
 # based on https://severalnines.com/database-blog/postgresql-deployment-and-maintenance-ansible
 
-cat > hosts <<EOF
+cat > inventory <<EOF
 [db]
-master  ansible_connection=local ansible_python_interpreter="/usr/libexec/platform-python"
-#slave1  ansible_host=192.168.4.3 ansible_user=vagrant ansible_password=vagrant
-#slave2  ansible_host=192.168.4.4 ansible_user=vagrant ansible_password=vagrant
+#master  ansible_connection=local ansible_python_interpreter="/usr/libexec/platform-python"
+master  ansible_host=192.168.4.2
 slave1  ansible_host=192.168.4.3
 slave2  ansible_host=192.168.4.4
 slave3  ansible_host=192.168.4.5
@@ -45,12 +44,28 @@ postgresql_max_connections: 150
 postgresql_pg_hba_custom:
    - { type: host,  database: all, user: all, address: "10.0.0.0/8",   method: "{{ postgresql_default_auth_method_hosts }}", comment: "Enable external connections:" }
    - { type: host,  database: all, user: all, address: "192.168.4.0/24",   method: "{{ postgresql_default_auth_method_hosts }}", comment: "Enable external connections:" }
+   - { type: host,  database: replication, user: all, address: "192.168.4.0/24",   method: "{{ postgresql_default_auth_method_hosts }}", comment: "Enable external connections:" }
+
+#host    replication      postgres       10.0.0.0/8            md5
+#host    all              postgres       10.0.0.0/8            md5
+
+#prepare for standby
+postgresql_wal_level: hot_standby
+postgresql_wal_log_hints: on
+postgresql_max_wal_senders: 8
+postgresql_wal_keep_segments: 64
+postgresql_hot_standby: on
 postgresql_users:
-   - name: user1
-     pass: 11111111
+   - name: replicate
+     pass: replicate123
 postgresql_databases:
-   - name: user1_db
-     owner: user1
+   - name: db01
+     owner: postgres
+postgresql_user_privileges:
+   - name: replicate
+     db: db01
+     priv: "ALL"
+     role_attr_flags: "CREATEDB"
 EOF
 
 cat > /home/vagrant/host_vars/slave1.yaml <<EOF
@@ -85,7 +100,7 @@ postgresql_pg_hba_custom:
 
 EOF
 
-cat > pg-m-s-s.yaml <<EOF
+cat > pg-m-s.yaml <<EOF
 - hosts: db
   become: yes
   pre_tasks:
@@ -110,14 +125,31 @@ cat > pg-m-s-s.yaml <<EOF
           - telnet
           - net-tools
      tags: software
+   - name: Add IP address of all hosts to all hosts
+     lineinfile:
+          dest: /etc/hosts
+          regexp: '.*{{ item }}$'
+          line: "{{ hostvars[item].ansible_host }} {{item}}"
+          state: present
+     when: hostvars[item].ansible_host is defined
+     with_items: "{{ groups.all }}"
+     tags: etc_hosts
   roles:
     - role: anxs.postgresql
 EOF
 
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
 
+sed -i -e "s|#ChallengeResponseAuthentication yes|ChallengeResponseAuthentication yes|g" /etc/ssh/sshd_config
+sed -i -e "s|ChallengeResponseAuthentication no|#ChallengeResponseAuthentication no|g" /etc/ssh/sshd_config
+service sshd restart
+
+echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.2 -f  -o StrictHostKeyChecking=no
 echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.3 -f  -o StrictHostKeyChecking=no
 echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.4 -f  -o StrictHostKeyChecking=no
 echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.5 -f  -o StrictHostKeyChecking=no
 
-ansible-playbook -i hosts pg-m-s-s.yaml 
+cat > pg-m-s.sh << EOF
+ansible-playbook -i inventory pg-m-s.yaml
+EOF
+sh pg-m-s.sh
