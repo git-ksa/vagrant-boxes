f81c5d5881cbe4eb059fc210cd1fbf555a88ecd8
Serg K <ksa@orsk.ru>
2020-01-30 11:42:56 +0300

    +ADD vbox CentOS7 install OpenStack via ansible (All in one) lxc

--- vbox/CentOS7_OpenStack_ansible_aio/scripts/install.sh
+++ vbox/CentOS7_OpenStack_ansible_aio/scripts/install.sh
@@ -1,11 +1,13 @@
+# based on
+#https://stafwag.github.io/blog/blog/2019/01/21/settinp-up-openstack-ansible-all-in-one-on-a-centos-7-system/
+#
 #https://computingforgeeks.com/resize-ext-and-xfs-root-partition-without-lvm/
-echo "=== resize root partition from 40GB to 100GB"
+echo "resize root partition from 40GB to 100GB"
 yum -y install cloud-utils-growpart
 growpart /dev/sda 1
 lsblk
 xfs_growfs /
 df -hT | grep /dev/sda
-echo "=== resize end"
 yum install git -y
 export PATH=/usr/local/bin:$PATH
 systemctl stop firewalld
@@ -13,8 +15,8 @@ systemctl disable firewalld
 iptables -L
 git clone https://git.openstack.org/openstack/openstack-ansible /opt/openstack-ansible
 cd /opt/openstack-ansible
-git branch -a
-git checkout stable/rocky
+#git branch -a
+#git checkout stable/rocky
 export ANSIBLE_FORCE_COLOR=true
 scripts/bootstrap-ansible.sh
 ls -ld /opt/*
@@ -26,5 +28,6 @@ openstack-ansible setup-hosts.yml
 openstack-ansible setup-infrastructure.yml
 openstack-ansible setup-openstack.yml
 netstat -pan | grep -i 443
+lxc-ls --fancy
 grep keystone_auth_admin_password /etc/openstack_deploy/user_secrets.yml
 #https://localhost:443
