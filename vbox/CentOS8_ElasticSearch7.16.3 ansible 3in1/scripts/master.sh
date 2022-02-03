89584c55c7477d550194289768803cd20e2f8f0f
Serg K <ksa@orsk.ru>
2022-02-03 09:15:09 +0300

    +CentOS8_ElasticSearch7.16.3 ansible 3in1

---
+++ vbox/CentOS8_ElasticSearch7.16.3 ansible 3in1/scripts/master.sh
@@ -0,0 +1,30 @@
+# Insall EPEL repository
+
+yum -y install epel-release
+yum -y install git ansible
+
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+ansible --version
+
+cp -r /vagrant/* /home/vagrant
+
+chown -R vagrant:vagrant /home/vagrant/
+
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+
+sed -i -e "s|#ChallengeResponseAuthentication yes|ChallengeResponseAuthentication yes|g" /etc/ssh/sshd_config
+sed -i -e "s|ChallengeResponseAuthentication no|#ChallengeResponseAuthentication no|g" /etc/ssh/sshd_config
+service sshd restart
+
+echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.2 -f  -o StrictHostKeyChecking=no
+echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.3 -f  -o StrictHostKeyChecking=no
+echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.4 -f  -o StrictHostKeyChecking=no
+
+cat > es.sh << EOF
+export ANSIBLE_FORCE_COLOR=true
+ansible-playbook -i inventory es_plb.yaml
+EOF
+
+sh es.sh
+
---
+++ vbox/CentOS8_ElasticSearch7.16.3 ansible 3in1/scripts/slave.sh
@@ -0,0 +1,5 @@
+#enable password auth
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+sed -i -e "s|#ChallengeResponseAuthentication yes|ChallengeResponseAuthentication yes|g" /etc/ssh/sshd_config
+sed -i -e "s|ChallengeResponseAuthentication no|#ChallengeResponseAuthentication no|g" /etc/ssh/sshd_config
+service sshd restart
