cf015ff09fba8c60a486438c4277df07c1b70c6a
Serg K <ksa@orsk.ru>
2022-07-24 13:02:10 +0300

    +AlmaLinux8_MongoDB6.0_ansible_auth 3in1

---
+++ vbox/AlmaLinux8_MongoDB6.0_ansible_auth/scripts/master.sh
@@ -0,0 +1,33 @@
+# Insall EPEL repository
+systemctl disable firewalld
+systemctl stop firewalld
+
+
+yum -y install epel-release
+#yum -y install git ansible python-psycopg2
+yum -y install git ansible 
+
+ansible --version
+echo root:vagrant | chpasswd
+
+cp -r /vagrant/* /home/vagrant
+
+chown -R vagrant:vagrant /home/vagrant/
+
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+
+sed -i -e "s|#ChallengeResponseAuthentication yes|ChallengeResponseAuthentication yes|g" /etc/ssh/sshd_config
+sed -i -e "s|ChallengeResponseAuthentication no|#ChallengeResponseAuthentication no|g" /etc/ssh/sshd_config
+service sshd restart
+
+echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.2 -f  -o StrictHostKeyChecking=no
+echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.3 -f  -o StrictHostKeyChecking=no
+echo "vagrant" | sshpass  ssh-copy-id root@192.168.4.4 -f  -o StrictHostKeyChecking=no
+
+cat > mongodb.sh << EOF
+export ANSIBLE_FORCE_COLOR=true
+ansible-playbook -i inventory mongodb_plb.yaml
+EOF
+
+sh mongodb.sh
+
---
+++ vbox/AlmaLinux8_MongoDB6.0_ansible_auth/scripts/slave.sh
@@ -0,0 +1,8 @@
+#enable password auth
+systemctl disable firewalld
+systemctl stop firewalld
+
+echo root:vagrant | chpasswd
+sed -i -e "s|#ChallengeResponseAuthentication yes|ChallengeResponseAuthentication yes|g" /etc/ssh/sshd_config
+sed -i -e "s|ChallengeResponseAuthentication no|#ChallengeResponseAuthentication no|g" /etc/ssh/sshd_config
+service sshd restart
